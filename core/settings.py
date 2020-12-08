c30d9fc2d1d64b7f56e633ab46f69bf0e5e8cb15
unknown <kuycoding@gmail.com>
2020-12-08 11:11:27 +0700

    update debug to true

--- core/settings.py
+++ core/settings.py
@@ -17,8 +17,8 @@ PROJECT_DIR = Path(__file__).parent
 SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False)
-DEBUG = False
+DEBUG = config('DEBUG', default=False)
+# DEBUG = False
 
 
 # load production server from .env
@@ -120,7 +120,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
